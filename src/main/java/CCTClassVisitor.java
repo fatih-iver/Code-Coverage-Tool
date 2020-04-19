83d066eb2a9ceb6161bce77928ca0c8cae8f16d1
Fatih İver <fatih.iver@boun.edu.tr>
2020-04-19 18:21:21 +0300

    add cct class visitor

---
+++ src/main/java/CCTClassVisitor.java
@@ -0,0 +1,9 @@
+import org.objectweb.asm.ClassVisitor;
+
+public class CCTClassVisitor extends ClassVisitor {
+
+    public CCTClassVisitor(int api, ClassVisitor classVisitor) {
+        super(api, classVisitor);
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,28 @@
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Code Coverage Tool");
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Code Coverage Tool - Start");
+
+        ClassWriter classWriter = new ClassWriter(0);
+
+        CCTClassVisitor cctClassVisitor = new CCTClassVisitor(Opcodes.ASM8, classWriter);
+
+        ClassReader classReader = new ClassReader("Calculator");
+
+        classReader.accept(cctClassVisitor, 0);
+
+        byte[] byteArray = classWriter.toByteArray();
+
+        System.out.println("Code Coverage Tool - End");
+
+
     }
+
 }
